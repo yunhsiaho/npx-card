8da5df86f6923d568215c79ba37e771a6390b8e1
hsia <yunhsia.ho@gmail.com>
2021-04-21 21:31:58 +0200

    add

--- index.js
+++ index.js
@@ -3,54 +3,65 @@
  *
  * /index.js - Main Entry point
  */
-
 const chalk = require("chalk");
 const boxen = require("boxen");
+const figlet = require("figlet");
+
+// const link = terminalLink('My Website', 'https://sindresorhus.com');
+// console.log(link);
 
 // Text + chalk definitions
 const data = {
-    name: chalk.white("             Pierre-Antoine Delnatte"),
-    handle: chalk.white("hsia"),
-    work: chalk.white("Code Guardian at BeCode.org"),
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("leny_be"),
-    npm: chalk.gray("https://npmjs.com/") + chalk.red("~leny"),
-    github: chalk.gray("https://github.com/") + chalk.green("leny"),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("padelnatte"),
-    web: chalk.cyan("https://leny.me"),
-    npx: chalk.red("npx") + " " + chalk.white("leny"),
-    labelWork: chalk.white.bold("       Work:"),
-    labelTwitter: chalk.white.bold("    Twitter:"),
-    labelNpm: chalk.white.bold("        npm:"),
-    labelGitHub: chalk.white.bold("     GitHub:"),
-    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-    labelWeb: chalk.white.bold("        Web:"),
-    labelCard: chalk.white.bold("       Card:"),
+    labelGitHub: chalk.yellow.bold("     GitHub:"),
+    labelLinkedIn: chalk.yellow.bold("   LinkedIn:"),
+
+    name: chalk.cyanBright(" 何韻霞 "),
+    handle: chalk.white("                     HO YUN-HSIA"),
+    work: chalk.white("      A little junior at BeCode Liege"),
+    github: chalk.gray("https://github.com/") + chalk.cyanBright("yunhsiaho"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.cyanBright("yunhsiaho"),
 };
 
 const NEWLINE = "\n";
 const EMPTYLINE = "";
+const attension = chalk.red.bold("                      Attension!");
+const wild = chalk.red.bold("The Wild");
+const isHere = chalk.red.bold("Is Here!!! (((ﾟДﾟ;))) Σ(ﾟДﾟ；≡；ﾟдﾟ)");
+// const face =  chalk.red.bold("           (((ﾟДﾟ;))) Σ(ﾟДﾟ；≡；ﾟдﾟ)");
 
 console.log(
-    chalk.green(
+    chalk.redBright(
         boxen(
             [
-                `${data.name} / ${data.handle}`,
+                `${attension}`,
                 EMPTYLINE,
-                `${data.labelWork} ${data.work}`,
+                `${wild} ${data.name}${isHere}`,
                 EMPTYLINE,
-                `${data.labelTwitter} ${data.twitter}`,
-                `${data.labelNpm} ${data.npm}`,
+                `${data.handle}`,
+                EMPTYLINE,
+                `${data.work}`,
                 `${data.labelGitHub} ${data.github}`,
                 `${data.labelLinkedIn} ${data.linkedin}`,
-                `${data.labelWeb} ${data.web}`,
-                EMPTYLINE,
-                `${data.labelCard} ${data.npx}`,
             ].join(NEWLINE),
             {
                 padding: 1,
                 margin: 1,
-                borderStyle: "round",
+                borderStyle: "double",
             },
         ),
     ),
+);
+figlet.text('npx hsia',{
+    font:'Ghost',
+    width:80,
+    whitespaceBreak:true
+},function(err, data){
+    if(err){
+        console.log('OOPS!Something wrong happened~');
+        console.log(err);
+        return;
+    }
+    console.log(data);
+}
+
 );
